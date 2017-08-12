2830e10be524e201647808695e1575527fa885a5
Koffi Sani <koffisani@gmail.com>
2017-08-12 23:36:33 +0100

    Added static pages app

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('greetings.urls')),
+    url(r'^hello', include('greetings.urls')),
+    url(r'^staticpages', include('staticpages.urls')),
 ]
